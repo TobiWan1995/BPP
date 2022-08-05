0af413a4e9fa0a060d296abee44a2e8f49a2abb7
Walter, Tobias <69347097+TobiWan123@users.noreply.github.com>
2022-08-05 18:37:20 +0200

    initial

---
+++ src/main/kotlin/com/example/bpp/StandardisierungRefactoredApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bpp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StandardisierungRefactoredApplication
+
+fun main(args: Array<String>) {
+	runApplication<StandardisierungRefactoredApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/bpp/StandardisierungRefactoredApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bpp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StandardisierungRefactoredApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
